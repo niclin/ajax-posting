59dd2718b3ef529fd32545700876f8ab514a11d4
Nic <niclin0226@gmail.com>
2017-04-04 21:02:11 +0800

    Ajax for like button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,16 +21,12 @@ class PostsController < ApplicationController
     unless @post.find_like(current_user)  # 如果已经按讚过了，就略过不再新增
       Like.create( :user => current_user, :post => @post)
     end
-
-    redirect_to posts_path
   end
 
   def unlike
     @post = Post.find(params[:id])
     like = @post.find_like(current_user)
     like.destroy
-
-    redirect_to posts_path
   end
 
   protected
