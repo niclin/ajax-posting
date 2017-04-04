dc60e595300bafb0d345c15f9990af2e0e23fe11
Nic <niclin0226@gmail.com>
2017-04-04 20:59:44 +0800

    Implement user can give posts like and dislike

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,23 @@ class PostsController < ApplicationController
     @post.destroy
   end
 
+  def like
+    @post = Post.find(params[:id])
+    unless @post.find_like(current_user)  # 如果已经按讚过了，就略过不再新增
+      Like.create( :user => current_user, :post => @post)
+    end
+
+    redirect_to posts_path
+  end
+
+  def unlike
+    @post = Post.find(params[:id])
+    like = @post.find_like(current_user)
+    like.destroy
+
+    redirect_to posts_path
+  end
+
   protected
 
   def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :posts
+  resources :posts do
+    member do
+      post "like" => "posts#like"
+      post "unlike" => "posts#unlike"
+    end
+  end
 
   get  "/jquery-1"  =>  "pages#jquery_1"
   get  "/jquery-2"  =>  "pages#jquery_2"
