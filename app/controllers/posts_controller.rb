011efa2c2fffb4b7964b768d03733bc90c963cfa
Nic <niclin0226@gmail.com>
2017-04-10 13:38:51 +0800

    Custom delete event

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,8 @@ class PostsController < ApplicationController
   def destroy
     @post = current_user.posts.find(params[:id]) # 只能删除自己的贴文
     @post.destroy
+
+    render :json => { :id => @post.id }
   end
 
   def like
